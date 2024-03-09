3960e2319ec253125e7e366c41f10331b00620ff
RakhimDeSanta <santaclousx1@gmail.com>
2024-03-09 09:32:26 +0500

    initial commit

---
+++ src/main/java/uz/pdp/gymfitnessapp/GymFitnessAppApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.gymfitnessapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymFitnessAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymFitnessAppApplication.class, args);
+    }
+
+}
