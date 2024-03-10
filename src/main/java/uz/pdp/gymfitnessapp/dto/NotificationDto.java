cac25e4e018bd010ebf0b18646d82f593552daf8
RakhimDeSanta <santaclousx1@gmail.com>
2024-03-10 23:49:39 +0500

    Notification added
    Notification added

---
+++ src/main/java/uz/pdp/gymfitnessapp/dto/NotificationDto.java
@@ -0,0 +1,18 @@
+package uz.pdp.gymfitnessapp.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class NotificationDto {
+    @NotEmpty
+    private UUID userId;
+    @NotEmpty
+    private String title;
+    @NotEmpty
+    private String text;
+}
