fb0b7b111967c045816f82000fe6b837a06c77b3
RakhimDeSanta <santaclousx1@gmail.com>
2024-04-02 00:30:19 +0500

    Attachment Done

--- src/main/java/uz/pdp/gymfitnessapp/common/AppConstants.java
+++ src/main/java/uz/pdp/gymfitnessapp/common/AppConstants.java
@@ -3,4 +3,5 @@ package uz.pdp.gymfitnessapp.common;
 public interface AppConstants {
     String BASE_PATH = "/api/v1";
     String TOKEN_TYPE = "Bearer ";
+    String BUCKET_NAME = "gym-fitness-app-bucket";
 }
